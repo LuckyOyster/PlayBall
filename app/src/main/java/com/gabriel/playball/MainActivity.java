b4fe91a7905db3f810a553027ad306da5ae5dd53
GoogleGabriel <LuckyOyster@163.com>
2018-03-13 22:45:31 +0800

    fix fullscreen

--- app/src/main/java/com/gabriel/playball/MainActivity.java
+++ app/src/main/java/com/gabriel/playball/MainActivity.java
@@ -1,22 +1,21 @@
 package com.gabriel.playball;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
-    private static int screenWidth;
-    private static int screenHeight;
+    private int screenWidth;
+    private int screenHeight;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //Set fullscreen
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
 
         //get screen width&height
@@ -24,10 +23,6 @@ public class MainActivity extends AppCompatActivity {
         getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
         screenWidth=displayMetrics.widthPixels;
         screenHeight=displayMetrics.heightPixels;
-
-        Toast toast=Toast.makeText(this,screenHeight+'*'+screenWidth,Toast.LENGTH_LONG);
-        toast.show();
-
 
 
     }
